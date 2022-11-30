7bac9b11c1415f84bf6d94d7b643fe9fced3bf29
Vanesa Soledad Garcia <vanesa.garcia@live.com.ar>
2022-11-29 22:20:07 -0300

    Segundo cambio

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
 import logo from './logo.svg';
+import logonav from './components/navbar/logo.png';
 import './App.css';
+import Navbar from './components/navbar';
 
 function App() {
   return (
     <div className="App">
+      <Navbar>
+        <img className = "logonav" src= {logonav} alt="Logo de The Beauty Bar"/>
+        <ul className = "menu">
+          <li className = "item">
+            <a href= "">Nosotros</a>
+          </li>
+          <li className = "item">
+            <a href= "">Productos</a>
+          </li>
+          <li className = "item">
+            <a href= "">Ubicacion</a>
+          </li>
+          <li className = "item">
+            <a href= "">Contactanos</a>
+          </li>
+        </ul>
+      </Navbar>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
