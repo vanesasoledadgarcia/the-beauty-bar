afca8d070bac00ccb9eb0331ae533f0f4753b5d8
Vanesa Soledad Garcia <vanesa.garcia@live.com.ar>
2023-01-14 23:40:14 -0300

    firebase

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,5 @@
 export {default as Sidebar} from './sidebar';
 export {default as Navbar} from './navbar';
 export {default as CartItem} from './cart-item';
+export {default as Loader} from './loader';
+export {default as Card} from './card';
--- src/pages/menu/index.jsx
+++ src/pages/menu/index.jsx
@@ -1,22 +1,55 @@
 import React from 'react';
 import './styles.css';
 import Card from '../../components/card';
-import {PRODUCTS} from '../../constants/data/products';
+//import {PRODUCTS} from '../../constants/data/products';
 import {useNavigate} from 'react-router-dom';
 import { getFireStore, doc, getDoc } from 'firebase/firestore';
+import Loader from '../../components/loader';
+import { useState } from 'react';
+import { useEffect } from 'react';
+import { getFirestore, collection, getDocs } from 'firebase/firestore';
 
 const Menu = () => {
+  const [loading, setLoading] = useState(true);
+  const [products, setProducts] = useState([]);
   const navigate = useNavigate();
   const onHandlerSelect = (product) => {
     navigate('/details/${product.id}', {state : product})
   }
+
+  useEffect (() => {
+    setLoading(true);
+    const db = getFirestore();
+    const products = collection(db, 'products');
+    getDocs(products)
+      .then((snapshot) => {
+        const result = snapshot.docs.map((doc) => (doc.data()))
+        setProducts(result);
+    })
+    .catch((error) => {
+      console.log(error)
+    })
+    .finally(() => {
+      setLoading(false);
+    })
+  }, []);
+
   return (
     <div className="container">
-      <div className = "products-container">
-        {PRODUCTS.map((product) => (
+      {loading ? (
+        <div className = "loading-container">
+          <Loader/>
+        </div>
+      ) : (
+        <>
+        <h1>Productos destacados</h1>
+        <div className = "products-container">
+        {products.map((product) => (
             <Card product = {product} key = {product.name} onSelect={onHandlerSelect}/>
         ))}
-      </div>
+        </div>
+        </>
+      )}
     </div>
   );
 }
